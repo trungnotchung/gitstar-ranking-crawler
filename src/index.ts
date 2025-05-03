2893fbdeb642b123107771dbb745aaa38995b22b
hungdungn47 <hungdungn47@gmail.com>
2025-05-03 23:11:06 +0700

    feat: attempt to fetch 5000 top repos

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-import { upsertRepoWithReleasesAndCommits } from "./dbService";
+import { PROXY_URL_1 } from "./config";
+import { paginatedFetchTopRepos } from "./crawlService";
+import { PrismaClient } from "./generated/prisma";
+import { GitHubRepo } from "./interfaces"; // interface { full_name: string }
 
+const prisma = new PrismaClient();
 
 (async () => {
   try {
-    // fetch top 5000 repositories and their releases and commits
-    // add them to the database
-    console.log("Data inserted successfully!");
+    const repos: GitHubRepo[] = await paginatedFetchTopRepos(5000, PROXY_URL_1);
+
+    for (const repo of repos) {
+      const [owner, name] = repo.full_name.split("/");
+      await prisma.repo.upsert({
+        where: { name_owner: { name, owner } },
+        update: {},
+        create: { name, owner },
+      });
+    }
+
+    console.log("✅ Data inserted successfully!");
   } catch (error) {
     console.error("❌ Error inserting data:", error);
+  } finally {
+    await prisma.$disconnect();
   }
 })();
